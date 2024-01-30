6e9fb5b10590f56293799704fd38a811e4b30565
Miron <popovmiron@gmail.com>
2024-01-30 17:45:19 +0700

    создание navbar

--- app/src/main/java/com/shift/cinemaapp/MainActivity.kt
+++ app/src/main/java/com/shift/cinemaapp/MainActivity.kt
@@ -10,37 +10,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.shift.cinemaapp.ui.theme.CinemaAppTheme
+import com.shift.cinemaapp.ui.theme.navBar
+import com.shift.cinemaapp.ui.theme.topBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+
             CinemaAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                NavHost(
+                    navController = navController,
+                    startDestination = "screen_1"
+                ){
+                    composable("screen_1"){
+                        navBar()
+                    }
+                    composable("screen_2"){
+                        topBar("fas")
+                    }
+                }
+                Surface {
+
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CinemaAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/shift/cinemaapp/ui/theme/Elements.kt
+++ app/src/main/java/com/shift/cinemaapp/ui/theme/Elements.kt
@@ -1,21 +1,106 @@
 package com.shift.cinemaapp.ui.theme
 
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.shift.cinemaapp.R
 
 data class BottomNavigationItem(
     val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
-    val hasNews: Boolean
+    val icon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
 )
 
 
-//@Composable
-//fun navBar(){
-//    Scaffold(NavigationBar {
-//
-//        }
-//    ) {
-//
-//    }
-//}
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+//@Preview
+fun navBar() {
+    val items = listOf(
+        BottomNavigationItem(
+            //title = R.string.afisha.toString(),
+            title = "Афиша",
+            icon = ImageVector.vectorResource(R.drawable.movie),
+            hasNews = false,
+            badgeCount = null
+        ),
+        BottomNavigationItem(
+            //title = R.string.tickets.toString(),
+            title = "Билеты",
+            icon = ImageVector.vectorResource(R.drawable.ticket),
+            hasNews = false,
+            badgeCount = null
+        ),
+        BottomNavigationItem(
+            //title = R.string.profile.toString(),
+            title = "Профиль",
+            icon = ImageVector.vectorResource(R.drawable.profile),
+            hasNews = false,
+            badgeCount = null
+        )
+    )
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = { selectedItemIndex = index },
+                        label = { Text(text = item.title) },
+                        icon = {
+                            BadgedBox(
+                                badge = {
+                                    if (item.badgeCount != null) {
+                                        Badge {
+                                            Text(text = item.badgeCount.toString())
+                                        }
+                                    } else if (item.hasNews) {
+                                        Badge()
+                                    }
+                                }
+                            ){
+                                Icon(imageVector = item.icon, contentDescription = item.title)
+                            }
+                        })
+                }
+            }
+        }
+    ) {}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+//@Preview
+fun topBar(title: String){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = title) }
+            )
+        }
+    )
+    {}
+}
